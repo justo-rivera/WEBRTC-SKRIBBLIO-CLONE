1435011726f09901405386b185b557b6ad39eaad
Justo Rivera <justogdrmonterrey@gmail.com>
2020-06-30 15:12:52 +0200

    typo

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_URL: process.env.API_URL || 'http://192.168.1.4/api',
-    PROFILE_URL: process.env.REACT_APP_PROFILE_URL || 'http://192.168.1.4/profile',
+    API_URL: process.env.REACT_APP_API_URL || 'http://192.168.1.4:5000/api',
+    PROFILE_URL: process.env.REACT_APP_PROFILE_URL || 'http://192.168.1.4:5000/profile',
     SOCKET_URL: process.env.REACT_APP_SOCKET_URL || 'http://192.168.1.4:5000/'
 }
